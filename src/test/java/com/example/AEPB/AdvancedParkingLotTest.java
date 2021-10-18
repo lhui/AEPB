14170153ad672ae6f59f1ce03a883f726231822f
wangchao <chao.wang2@thoughtworks.com>
2021-10-18 21:39:53 +0800

    [wangchao] feat: add tasking 2.

--- src/test/java/com/example/AEPB/AdvancedParkingLotTest.java
+++ src/test/java/com/example/AEPB/AdvancedParkingLotTest.java
@@ -1,5 +1,6 @@
 package com.example.AEPB;
 
+import com.example.AEPB.parkinglot.Car;
 import com.example.AEPB.parkinglot.Ticket;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -16,12 +17,22 @@ class AdvancedParkingLotTest {
     @Test
     void should_park_car_and_get_ticket_successfully_when_parking_car_given_ten_empty_parkingLots_and_parking_boy_and_one_parking_car() {
         ParkingLotGroup parkingLotGroup = new ParkingLotGroup();
+        Car car = new Car();
         Ticket ticket = parkingLotGroup.parkingCarAndGetTicketByParkingBoy(car);
-        Assertions.assertEquals(1, parkingLotGroup.getTicketCountByParkingNumber(1));
-        Assertions.assertEquals(49, parkingLotGroup.getParkingLotSpaceByParkingNumber(1));
-        Assertions.assertEquals(50, parkingLotGroup.getParkingLotSpaceByParkingNumber(2));
-        Assertions.assertEquals(UUID.randomUUID().toString().length(), ticket.getTicketNumber().length());
+        Assertions.assertEquals(car, parkingLotGroup.getCarByTicketAndParkingBoy(ticket));
     }
 
-
+    /*
+     * 2、
+     * given 10个停车场都未停车，自停1号停车场，一辆车
+     * when 停车
+     * then 停车成功并获得车票
+     * */
+    @Test
+    void should_park_car_and_get_ticket_successfully_when_parking_car_given_ten_empty_parkingLots_and_self_parking_No1_parkingLot_and_one_parking_car() {
+        ParkingLotGroup parkingLotGroup = new ParkingLotGroup();
+        Car car = new Car();
+        Ticket ticket = parkingLotGroup.parkingCarAndGetTicketBySelfParking(car, 1);
+        Assertions.assertEquals(car, parkingLotGroup.getCarByTicketAndParkingBoy(ticket));
+    }
 }
