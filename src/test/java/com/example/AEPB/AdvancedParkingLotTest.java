7d4082f0cf32e16e95a3091a76301c80d7f7cba0
wangchao <chao.wang2@thoughtworks.com>
2021-10-19 10:13:42 +0800

    [wangchao] feat: add should_get_car_failed_and_throw_exception_when_take_the_car_given_No1_parkingLot_has_one_cars_and_self_take_No1_parkingLot_and_one_invalid_ticket test.

--- src/test/java/com/example/AEPB/AdvancedParkingLotTest.java
+++ src/test/java/com/example/AEPB/AdvancedParkingLotTest.java
@@ -166,7 +166,7 @@ class AdvancedParkingLotTest {
      * then 取车失败并抛异常
      * */
     @Test
-    void should_get_car_successfully_when_take_the_car_given_No1_parkingLot_has_one_cars_and_self_take_No2_parkingLot_and_one_matched_ticket_for_No1_parkingLot() {
+    void should_get_car_failed_and_throw_exception_when_take_the_car_given_No1_parkingLot_has_one_cars_and_self_take_No2_parkingLot_and_one_matched_ticket_for_No1_parkingLot() {
         ParkingLotGroup parkingLotGroup = new ParkingLotGroup();
         Car car = new Car();
         Ticket ticket = parkingLotGroup.parkingCarAndGetTicketByParkingBoy(car);
@@ -180,7 +180,7 @@ class AdvancedParkingLotTest {
      * then 取车成功
      * */
     @Test
-    void should_get_car_successfully_when_take_the_car_given_No1_parkingLot_has_one_cars_and_parking_boy_and_one_matched_ticket() {
+    void should_get_car_failed_and_throw_exception_when_take_the_car_given_No1_parkingLot_has_one_cars_and_parking_boy_and_one_matched_ticket() {
         ParkingLotGroup parkingLotGroup = new ParkingLotGroup();
         Car car = new Car();
         Ticket ticket = parkingLotGroup.parkingCarAndGetTicketByParkingBoy(car);
@@ -193,6 +193,13 @@ class AdvancedParkingLotTest {
      * when 取车
      * then 取车失败并抛异常
      * */
+    @Test
+    void should_get_car_failed_and_throw_exception_when_take_the_car_given_No1_parkingLot_has_one_cars_and_self_take_No1_parkingLot_and_one_invalid_ticket() {
+        ParkingLotGroup parkingLotGroup = new ParkingLotGroup();
+        Car car = new Car();
+        Ticket ticket = parkingLotGroup.parkingCarAndGetTicketByParkingBoy(car);
+        Assertions.assertThrows(TicketNotFoundCarException.class, () -> parkingLotGroup.getCarByTicketAndSelfParking(null, 1));
+    }
 
     /*
      * 13、
