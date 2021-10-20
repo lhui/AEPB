1450c741293ac7344df6cf3c32524816447d0b40
LiHui <hui.li@thoughtworks.com>
2021-10-20 22:04:43 +0800

    [lihui] feat: submit part Tasking of smartParkingBoy

---
+++ src/test/java/com/example/AEPB/SmartParkingBoyTest.java
@@ -0,0 +1,22 @@
+package com.example.AEPB;
+
+public class SmartParkingBoyTest {
+    /*
+     * given 1号停车场停有1辆车，2号停车场停有2两辆车，其他停车场为空，smart Parking boy，一辆车
+     * when 停车
+     * then 停车成功并获得车票，停在了3号停车位
+     * */
+
+    /*
+     * given 所有停车场都有0辆车，smart boy，一辆车
+     * when 停车
+     * then  停车成功并获得车票，停在了1号停车位
+     * */
+
+    /*
+     * given 所有停车场都有满，smart boy，一辆车
+     * when 停车
+     * then  停车失败并抛异常
+     * */
+
+}
