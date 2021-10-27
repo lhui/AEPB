47331e1eb0a9d78f275768846a569cc1c18efa55
wangchao <chao.wang2@thoughtworks.com>
2021-10-27 20:22:54 +0800

    [wangchao] feat: add parkingRobot test for parking at the biggest vacancy rate parking lot

---
+++ src/test/java/com/example/AEPB/ParkingRobotTest.java
@@ -0,0 +1,57 @@
+package com.example.AEPB;
+
+import com.example.AEPB.parkinglot.Car;
+import com.example.AEPB.parkinglot.ParkingLot;
+import com.example.AEPB.parkinglot.Ticket;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class ParkingRobotTest {
+    /*
+     * 1.
+     * given 两个停车场，1号停车场10个停车位停有1辆车，2号停车场30个停车位停有2两辆车，ParkingRobot，一辆车
+     * when 停车
+     * then 停车成功并获得车票，停在了2号停车位
+     * */
+    @Test
+    void should_park_car_and_get_ticket_successfully_when_parking_car_given_two_parking_lot_and_no1_parking_lot_has_10_parking_space_one_car_parked_and_no2_parking_lot_has_30_parking_space_two_car_parked_and_parking_robot_and_one_car(){
+        ParkingLot smallerVacancyRateParkingLot = new ParkingLot(10);
+        ParkingLot biggestVacancyRateParkingLot = new ParkingLot(30);
+        smallerVacancyRateParkingLot.parkingCarAndGetTicket(new Car());
+        biggestVacancyRateParkingLot.parkingCarAndGetTicket(new Car());
+        biggestVacancyRateParkingLot.parkingCarAndGetTicket(new Car());
+        Map<String, ParkingLot> parkingLotMap = new HashMap<>();
+        parkingLotMap.put("1", smallerVacancyRateParkingLot);
+        parkingLotMap.put("2", biggestVacancyRateParkingLot);
+        ParkingRobot parkingRobot = new ParkingRobot(parkingLotMap);
+        Car newCar = new Car();
+        Ticket ticket = parkingRobot.parkingCar(newCar);
+        Assertions.assertEquals(newCar, parkingLotMap.get("2").getCar(ticket));
+    }
+
+    /*
+     * 2.
+     * given 三个停车场，1号停车场10个停车位没有停车，2号停车场20个停车位没有停车，3号停车场20个停车位没有停车，ParkingRobot，一辆车
+     * when 停车
+     * then 停车成功并获得车票，停在了2号停车位
+     * */
+    @Test
+    void should_park_car_and_get_ticket_successfully_when_parking_car_given_three_parking_lot_and_no1_parking_lot_has_10_empty_parking_space_and_no2_parking_lot_has_20_empty_parking_space_and_no3_parking_lot_has_20_empty_parking_space_and_parking_robot_and_one_car(){
+
+    }
+
+    /*
+     * 2.
+     * given 两个停车场，1号停车场10个停车位停满，2号停车场20个停车位停满，ParkingRobot，一辆车
+     * when 停车
+     * then 停车失败并抛异常
+     * */
+    @Test
+    void should_park_car_fail_and_throw_exception_when_parking_car_given_two_parking_lot_and_no1_parking_lot_has_10_full_parking_space_and_no2_parking_lot_has_20_full_parking_space_and_parking_robot_and_one_car(){
+
+    }
+
+}
